439b1e0dfb21acef1cdd055ff9c29f936deaf4fb
arnold_therigan <arnoldtherigan15@gmail.com>
2019-11-14 21:02:46 +0700

    player can move

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,48 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Swal from 'sweetalert2'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    player: {
+      position: 1,
+      image: 'https://image.flaticon.com/icons/svg/47/47058.svg'
+    },
+    isWinner: false,
+    obstacles: [
+      {
+        start: 29,
+        end: 18
+      },
+      {
+        start:31,
+        end:4
+      }
+    ]
   },
   mutations: {
+    CHANGE_POSITION (state, payload) {
+      state.player.position = payload
+    },
+    ROLE_DICE (state) {
+      let randomNum = Math.floor(Math.random() * 13)
+      let newPosition = state.player.position += randomNum
+      if (newPosition === 100) {
+        state.isWinner = true
+        Swal.fire('success', 'you win', 'success')
+      } else if (newPosition < 100) {
+        state.player.position = newPosition
+      } else {
+        state.player.position = (100 - (newPosition - 100))
+      }
+    },
+    SET_OBSTACLE (state) {
+
+      state.obstacle = randomPos
+      console.log(state.obstacle,'-----------');
+    }
   },
   actions: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- <div class="home"> -->
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!-- </div> -->
+  <div class="container">
+
+<h1>Snakes and Ladders</h1>
+<div>
+  <Box v-for="(board,index) in boards " :key='index' :number='board'></Box>
+  <!-- <img class="snake1" src="../../public/snake2.png" alt="" width="250px;"> -->
+</div>
+<button v-if="!isWinner" @click="roledice">Dice Role</button>
+<!-- <img src="https://image.flaticon.com/icons/svg/84/84073.svg"> -->
+</div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Box from '@/components/Box'
+import { mapState } from 'vuex'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Box
+  },
+  data () {
+    return {
+      boards: [],
+      image: ''
+    }
+  },
+  created () {
+    for (let i = 100; i > 0; i--) {
+      this.boards.push(i)
+    }
+  },
+  computed: {
+    ...mapState([
+      'player','isWinner'
+    ])
+  },
+  methods: {
+    roledice () {
+      this.$store.commit('ROLE_DICE')
+      console.log(this.player.position)
+    }
   }
 }
 </script>
+
+<style scoped>
+.snake1 {
+  position: absolute !important;
+  top: 380px !important;
+  left: 220px !important;
+  z-index: 1 !important;
+}
+</style>
